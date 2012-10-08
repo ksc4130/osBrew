32846b308530b4dfd557af5dd527d3e4a47504af
Kyle <ksc4130@gmail.com>
2012-10-08 16:58:47 -0400

    about and new home start

--- public/scripts/viewmodels/index.js
+++ public/scripts/viewmodels/index.js
@@ -1,110 +1,15 @@
-var socket = io.connect();
+var _posts = ko.observableArray([
+	{
+		title: 'One',
+		date: new Date().toLocaleDateString().split(","),
+		content: 'content One'
+	}
+]);
 
-socket.on('updateTeam', function(data) {
-
-});
-
-var dragging;
-
-var teams = ko.observableArray([]);
-
-var vm  = function() {
-	var _playAfterRenderHandler = function(els, data) {
-		var $els = $(els);
-		if(data.top) {
-			$els.css({ top: data.top });
-		}
-		if(data.left) {
-			$els.css({ left: data.left });
-		}
-	};
+var vm = function() {
 	return {
-		teams: teams,
-		playAfterRenderHandler: _playAfterRenderHandler
+		posts: _posts
 	}
 }();
 
-var playerModel = function() {
-	if(this === window) {
-		return new playerModel(arguments);
-	}
-	var self = this;
-	var args = arguments[0]._id ? arguments[0] : arguments[0][0];
-	if(typeof(args) === 'undefined') {
-		args = {};
-	}
-	self._id = args._id || '';
-	self.fname = ko.observable(args.fname || '');
-	self.lname = ko.observable(args.lname || '');
-	self.left = args.left || '';
-	self.top = args.top || '';
-	self.isEdit = ko.observable(false);
-	self.edit = function() {
-		self.isEdit(!self.isEdit());
-	}
-};
-
-var teamModel = function() {
-	if(this === window) {
-		return new teamModel(arguments);
-	}
-	var self = this;
-	var args = arguments[0]._id ? arguments[0] : arguments[0][0];
-	if(typeof(args) === 'undefined') {
-		args = {};
-	}
-	self._id = args._id || '';
-	self.name = args.name || '';
-	self.players = ko.observableArray([]);
-	self.players.subscribe(function(v, e) {
-		console.log(ko.toJSON(self));
-		socket.emit('teamUpdate', ko.toJSON(self));
-	});
-	if(args.players && args.players.length > 0) {
-		for(var i = 0; i < args.players.length; i++) {
-			self.players.push(playerModel(args.players[i]));
-		}
-	}
-	self.addPlayer = function() {
-		self.players.push(playerModel());
-	};
-};
-
-data = $.parseJSON(data);
-for(var i = 0; i < data.length; i++) {
-	teams.push(teamModel(data[i]));
-}
-
-ko.bindingHandlers.player = {
-	init: function(el, val, allVal, vm, bc) {
-		var $el = $(el);
-		$el.draggable({
-			revert: 'invalid',
-			start: function(e, ui) {
-				dragging = { bc: bc, $el: $el };
-			}
-		});
-	}
-};
-
-ko.bindingHandlers.team = {
-	init: function(el, val, allVal, vm ,bc) {
-		var $el = $(el);
-		$el.droppable({
-			accept: '.player',
-			drop: function(e, ui) {
-				if(dragging.bc.$parent !== bc.$data) {
-					//changed teams
-					dragging.bc.$data.left = ui.offset.left - e.target.offsetLeft;
-					dragging.bc.$data.top = ui.offset.top - e.target.offsetTop;
-					dragging.bc.$parent.players.remove(dragging.bc.$data);
-					bc.$data.players.push(dragging.bc.$data);
-				}
-			}
-		});
-	}
-};
-
-$(function() {
-	ko.applyBindings(vm);
-});
+ko.applyBindings(vm);
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,16 @@
 
 exports.index = function(req, res){
-  res.render('index', { title: 'osBrew', active: 'home' });
+  res.render('index', { title: 'osBrew-Home', active: 'home' });
 };
 
 exports.gallery = function(req, res){
-  res.render('gallery', { title: 'osBrew', active: 'gallery' });
+  res.render('gallery', { title: 'osBrew-Gallery', active: 'gallery' });
+};
+
+exports.about = function(req, res){
+  res.render('about', { title: 'osBrew-About', active: 'about' });
 };
 
 exports.contact = function(req, res){
-  res.render('contact', { title: 'osBrew', active: 'contact' });
+  res.render('contact', { title: 'osBrew-Contact', active: 'contact' });
 };
